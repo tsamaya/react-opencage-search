d7488675ff794c271af12e372cba92f1d09dbe3c
Arnaud Ferrand <arferrand@gmail.com>
2021-01-10 13:33:26 +0100

    working on example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import { OpenCageSearch } from 'react-opencage-search'
-import 'react-opencage-search/dist/index.css'
+import { OpenCageSearch } from 'react-opencage-search';
+import 'react-opencage-search/dist/index.css';
+
+const OPENCAGE_API_KEY_TEST = '6d0e711d72d74daeb2b0bfd2a5cdfdba';
 
 const App = () => {
-  return <OpenCageSearch name={'address'} placeholder={'type an address'} apiKey={'4372eff77b8343cebfc843eb4da4ddc4'} />
-}
 
-export default App
+  const [apiKey, setApiKey] = useState(OPENCAGE_API_KEY_TEST)
+  const handleChangeApiKey = (e)=>{
+    setApiKey(e.target.value)
+  }
+  return (
+    <section class="section">
+ <div class="container">
+ <div class="field">
+  <div class="control">
+     <input name="api_key" class="input is-primary" type="text"  placeholder="Your API Key" value={apiKey} onChange={handleChangeApiKey} />
+     </div>
+</div>
+    <OpenCageSearch
+      name={'address'}
+      className="input is-info"
+      placeholder={'Address ord coordinates'}
+      apiKey={OPENCAGE_API_KEY_TEST}
+    />
+
+ </div>
+
+    </section>
+
+  );
+};
+
+export default App;
